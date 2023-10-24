2aa3bf17adb3d47de2d14c16addaacdbdf43da00
Ana Lilia Cruz Roque <isc20350264@gmail.com>
2023-10-24 12:59:21 -0600

    Contraseña cifrada

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const {request, response} = require('express');
+const bcrypt = require('bcrypt')
 const usersModel = require('../models/users');
 const pool = require('../db');
 
@@ -81,7 +82,10 @@ const addUser = async (req = request, res = response) => {
         return;
     }
 
-    const user = [username, email, password, name, lastname, phone_number, role_id, is_active]
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash (password,saltRounds);
+
+    const user = [username, email, passwordHash, name, lastname, phone_number, role_id, is_active]
 
     let conn;
 
